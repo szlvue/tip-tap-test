8a84eba270450a68e84585369b94d719b7418789
SZLVue <szlvue@gmail.com>
2020-02-01 08:06:17 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HelloWorld>
+      Something bad happened.
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/sass/variables.scss";
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $primary-text-color;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="demo-alert-box">
+      <strong>Error!</strong>
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/sass/variables.scss";
+
+  .demo-alert-box {
+    border: 1px solid $primary-text-color;
+    max-width: 40%;
+    min-height: 150px;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 30px;
+  }
+</style>
